27997c52815120ae252a0929556bcf74a214231c
awaliyah-creator <alifhiaawaliyah@gmail.com>
2025-12-17 15:27:55 +0700

    perubahan lagi

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('/sapa/{nama?}', function ($nama = 'Semua') {
 Route::get('/produk/{id}', function ($id) {
     return "Detail produk #$id";
 })->name('produk.detail');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
